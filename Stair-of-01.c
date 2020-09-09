b0bd40eabdb8536176c863dcd2b99cd0fe6f5048
Shibangi-Poly <68492053+Shibangi-Poly@users.noreply.github.com>
2020-09-09 21:37:49 +0530

    Created Stair-of-01.c
    Prints a stair of 01 s according to user's wish of number of rows.

---
+++ Stair-of-01.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+	int i, j, n;
+	/*Input number of rows from user*/
+	printf("Enter the number of rows: ");
+	scanf("%d", &n);
+	for( i=1; i<=n; i++)
+	{
+		for(j=1; j<=i; j++)
+		{
+			printf("01");
+		
+		}
+		printf("\n");
+	}
+	return 0;
+}
